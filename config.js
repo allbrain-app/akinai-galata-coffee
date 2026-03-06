2d5d4f86c5bffd575625e37c0fd0148b20c9ab65
allbrain-app <shuhei.kimura@allbrain.one>
2026-03-06 15:23:44 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 設定ファイル
 const MY_LIFF_ID = "2008984506-BwmXmwno";
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzLH0bfhcdOc4xmFfvfk9411_z26mvqZ_1MAgXAfwRi0Ug4ReCPqs7CbuyZ7hjxW1Kg/exec";
+const GAS_API_URL = "https://jwbzcfnxbcsouckxfeju.supabase.co/functions/v1/api";
